a9d856380050557dafe51118dd957e6fd0d4a323
Alhuin <julien.janinr@protonmail.com>
2020-05-28 21:47:45 +0200

    csvreader

--- ex03/CsvReader.py
+++ ex03/CsvReader.py
@@ -7,13 +7,19 @@ class CsvReader(object):
         self.file = None
         if isinstance(filename, str) and Path(filename).is_file():
             self.header = header
+            self.head = None
+            self.skip_top = skip_top
             self.file = open(filename, "r")
             data = self.file.read().split('\n')
             self.lines = [
                 line.split(sep)
                 for i, line in enumerate(data)
-                if skip_top <= i < len(data) - skip_bottom - 1
+                if skip_top < i < len(data) - skip_bottom - 1
             ]
+            if header:
+                self.head = data[0].split(sep)
+                if len(self.head) != len(self.lines[0]):
+                    self.file = None
             if not all(len(line) == len(self.lines[0]) for line in self.lines):
                 self.file = None
 
@@ -27,10 +33,12 @@ class CsvReader(object):
             self.file.close()
 
     def getdata(self):
-        return self.lines[1:] if self.header else self.lines
+        if self.header and self.skip_top == 0:
+            return self.lines[1:]
+        return self.lines
 
     def getheader(self):
-        return self.lines[0] if self.header else None
+        return self.head
 
 
 def main():
